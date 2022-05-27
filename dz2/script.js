a9033c7fdfb7bdfd9670b5b59a2b0c7df50b67ce
Aleks <zippon28@gmail.com>
2022-05-27 22:44:03 +0300

    task 8 completed

---
+++ dz2/script.js
@@ -0,0 +1,112 @@
+// Задание N1
+
+/*let name = prompt('Ваше имя'),
+    age = prompt('Возраст');
+    city = prompt('Город проживания'),
+    phone = prompt('Номер телефрна'),
+    email = prompt('Электронный адрес'),
+    company = prompt('Где Вы работаете?');
+
+alert('Меня зовут ' + name + '. Мне ' + age + ' лет. Я проживаю в городе ' + city + ' и работаю в компании ' + company + '. Мои контактные данные: ' + phone + ', ' + email + '.');*/
+
+// Задание N2
+
+/*let birth = (2022 - age);
+
+alert(name + ' родился в ' + birth + ' году.');*/
+
+// Задание N3
+
+/*let num1 = 4,
+    num2 = 3,
+    num3 = 5,
+    num4 = 2,
+    num5 = 9,
+    num6 = 1;
+
+if (num1 + num2 + num3 == num4 + num5 + num6) {alert('Да')} else {alert('Нет')};*/
+
+// Задание N4
+
+/*let a = prompt('Чему равно значение -a');
+let access = (a > 0) ? 'Верно' : 'Неверно';
+
+console.log(access);*/
+
+// Задание N5
+
+/*let a = 10,
+    b = 2,
+    c1 = a + b,
+    c2 = a - b,
+    c3 = a * b,
+    c4 = a / b;
+
+console.log(a + '+' + b + '=' + c1);
+console.log(a + '-' + b + '=' + c2);
+console.log(a + '*' + b + '=' + c3);
+console.log(a + '/' + b + '=' + c4);
+
+if (c1 > 1) {console.log(c1 * c1)};
+if (c2 > 1) {console.log(c2 * c1)};
+if (c3 > 1) {console.log(c3 * c1)};
+if (c4 > 1) {console.log(c4 * c1)};*/
+
+// Задание N6
+
+/*if (a > 2 && a < 11 || b >= 6 && b < 14) {
+    console.log('Верно')
+} else {
+    console.log('Неверно')
+}*/
+
+// Задание N7
+
+/*let n = 23;
+
+if (n <= 15) {
+    console.log('1-я четверть часа');
+} else if (n <= 30) {
+    console.log('2-я четверть часа');
+} else if (n <= 45) {
+    console.log('3-я четверть часа');
+} else {
+    console.log('4-я четверть часа');
+}*/
+
+// Задание N8
+
+let day = 30;
+
+/*if (day <= 10) {
+    document.write('1-я декада месяца');
+} else if (day <= 20) {
+    document.write('2-я декада месяца');
+} else {
+    document.write('3-я декада месяца');
+}*/
+
+// Задание N9
+
+/*let year = 365,
+    month = 31,
+    week = 7,
+    hour = 0.04166667,
+    min = hour * 0.01666667,
+    sec = min * 0.01666667;
+
+console.log(year, month, week, hour, min, sec);
+
+let x = 267;
+
+switch (x) {
+    case (267):
+        console.log('Меньше года');
+    break;
+}*/
+
+// Задание N10
+
+switch (day) {
+    case ()
+}
